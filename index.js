e5c3432e01365c07bf69ae7f4511726928355146
Misael Ordoñez <152438645+Misael-coder@users.noreply.github.com>
2024-09-20 11:29:54 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const button = document.querySelector('input[type="button"]');
+button.addEventListener('click',checkCode)
+button.addEventListener('click', contentvisible);
+
+
+function contentvisible(){
+  const contetPrincial = document.getElementById('content-principal')
+  contetPrincial.style.display = 'none'
+}
+
+function checkCode() {
+  const inputCode = document.querySelector('#input-codigo').value.trim();
+  const articles = document.querySelectorAll('article');
+
+  const codes = {
+    '9 de julio': 'codigo-uno', // nuestro primer beso
+    'En el parque': 'codigo-dos', //donde fue nuestro primer beso
+    '2020': 'codigo-tres', // año que nos conosimos
+    '20 de septriembre': 'codigo-cuatro' // aniversario
+  };
+
+  if (codes.hasOwnProperty(inputCode)) {
+    const articleId = codes[inputCode];
+    const article = document.getElementById(articleId);
+    article.style.display = 'block';
+  } else {
+    alert('Invalid code');
+  }
+}
